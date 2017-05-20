d9512f7ea5a910041b925d10be9015f1a71fca01
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-20 18:25:25 -0300

    Add kotlin and create main activity

---
+++ app/src/main/java/com/example/bloder/rxmvp/MainFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bloder.rxmvp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainFoodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_food)
+    }
+}
