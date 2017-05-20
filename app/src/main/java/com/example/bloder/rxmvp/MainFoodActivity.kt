9304d5027f41e06c25f0e36d77d8cf38ca8c7ad7
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-20 18:39:03 -0300

    Bind main activity views

--- app/src/main/java/com/example/bloder/rxmvp/MainFoodActivity.kt
+++ app/src/main/java/com/example/bloder/rxmvp/MainFoodActivity.kt
@@ -2,8 +2,13 @@ package com.example.bloder.rxmvp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.widget.FrameLayout
 
 class MainFoodActivity : AppCompatActivity() {
+
+    private val tab       by lazy { findViewById(R.id.tab) as TabLayout }
+    private val container by lazy { findViewById(R.id.container) as FrameLayout }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
