aa630f16d1b5c6d24d38188edbbb4a8391b374a9
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-21 12:44:47 -0300

    Create food representer class

---
+++ app/src/main/java/com/example/bloder/rxmvp/home/representers/FoodRepresenter.kt
@@ -0,0 +1,9 @@
+package com.example.bloder.rxmvp.home.representers
+
+import com.example.bloder.rxmvp.home.identifiers.FoodId
+import com.example.bloder.rxmvp.rx.RxFood
+
+/**
+ * Created by bloder on 20/05/17.
+ */
+class FoodRepresenter(val id: FoodId) : RxFood.Representer
