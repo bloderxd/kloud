d7497704a793ad205a2d070007207ffcc730dbc7
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-21 12:42:41 -0300

    Create food data class

---
+++ app/src/main/java/com/example/bloder/rxmvp/data/Food.kt
@@ -0,0 +1,10 @@
+package com.example.bloder.rxmvp.data
+
+/**
+ * Created by bloder on 21/05/17.
+ */
+data class Food(
+        val name: String = "",
+        val description: String = "",
+        val image: String = ""
+)
