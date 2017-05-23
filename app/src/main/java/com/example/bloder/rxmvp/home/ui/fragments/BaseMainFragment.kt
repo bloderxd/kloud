25dc797cbe5fd1cf9f455268c5fc4b97b2ce2594
Daniel <wowmegadenis@hotmail.com>
2017-05-23 02:53:45 -0300

    Create base main fragment

---
+++ app/src/main/java/com/example/bloder/rxmvp/home/ui/fragments/BaseMainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.bloder.rxmvp.home.ui.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bloder.rxmvp.R
+
+/**
+ * Created by bloder on 22/05/17.
+ */
+open class BaseMainFragment : Fragment() {
+
+    private val foodList by lazy { view?.findViewById(R.id.food_list) as RecyclerView }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.base_fragment, container)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
