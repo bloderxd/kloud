d542f22920114a52bd0ca05ea2a82ff7de797801
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-23 17:30:15 -0300

    Change food interactor implementation

--- app/src/main/java/com/example/bloder/rxmvp/home/arch/FoodInteractor.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/arch/FoodInteractor.kt
@@ -1,11 +1,13 @@
 package com.example.bloder.rxmvp.home.arch
 
-import com.example.bloder.rxmvp.data.Food
+import com.example.bloder.rxmvp.base_arch.mvp.RxBasePresenter
 
 /**
  * Created by bloder on 20/05/17.
  */
-class FoodInteractor : FoodContract.Interactor {
+class FoodInteractor(override var presenter: RxBasePresenter<*>) : FoodContract.Interactor {
 
-    override fun fetchFood(): Food = Food()
+    override fun fetchFood() {
+        // FETCH FOOD
+    }
 }
