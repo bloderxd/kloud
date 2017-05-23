4cc31a8ae8143eb35854c58cbb22cab485a092a1
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-23 17:32:25 -0300

    Implement fetch foods action context in representr

--- app/src/main/java/com/example/bloder/rxmvp/home/representers/FoodRepresenter.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/representers/FoodRepresenter.kt
@@ -1,10 +1,17 @@
 package com.example.bloder.rxmvp.home.representers
 
-import android.support.v4.app.Fragment
+import com.example.bloder.rxmvp.data.Food
 import com.example.bloder.rxmvp.home.identifiers.FoodId
-import com.example.bloder.rxmvp.rx.RxFood
+import com.example.bloder.rxmvp.rx.Cloud
 
 /**
  * Created by bloder on 20/05/17.
  */
-class FoodRepresenter(val id: FoodId, val ref: Fragment) : RxFood.Representer
+data class FoodRepresenter(val id: FoodId) : Cloud.Representer {
+
+    var foods: List<Food> = listOf()
+
+    constructor(id: FoodId, foods: List<Food>) : this(id) {
+        this.foods = foods
+    }
+}
