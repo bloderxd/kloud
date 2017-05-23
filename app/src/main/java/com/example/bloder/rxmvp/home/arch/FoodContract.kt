496918f63edab2e3a1b0591222f0951c0a5cb22d
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-23 17:28:48 -0300

    Create food fragment view arch

--- app/src/main/java/com/example/bloder/rxmvp/home/arch/FoodContract.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/arch/FoodContract.kt
@@ -1,25 +1,32 @@
 package com.example.bloder.rxmvp.home.arch
 
-import com.example.bloder.rxmvp.base_arch.RxBasePresenter
-import com.example.bloder.rxmvp.base_arch.RxBaseView
+import com.example.bloder.rxmvp.base_arch.mvp.BaseInteractor
+import com.example.bloder.rxmvp.base_arch.mvp.RxBasePresenter
+import com.example.bloder.rxmvp.base_arch.mvp.view.StateKeeperRxBaseView
+import com.example.bloder.rxmvp.base_arch.mvp.view.StateProviderRxBaseView
 import com.example.bloder.rxmvp.data.Food
-import com.example.bloder.rxmvp.home.identifiers.ChildId
+import com.example.bloder.rxmvp.home.identifiers.fragments.FoodChildViewId
+import com.example.bloder.rxmvp.home.identifiers.state.ChildId
 
 /**
  * Created by bloder on 20/05/17.
  */
 class FoodContract {
 
-    interface View : RxBaseView<ChildId> {
-        fun onFoodFetched()
+    interface View : StateKeeperRxBaseView<ChildId> {
+        fun onFoodFetched(foods: List<Food>)
+    }
+
+    interface FoodView : StateProviderRxBaseView<FoodChildViewId> {
+        fun onFoodFetched(foods: List<Food>)
     }
 
     interface Presenter<T> : RxBasePresenter<T> {
         fun fetchFood()
-        fun onFoodFetched()
+        fun onFoodFetched(foods: List<Food>)
     }
 
-    interface Interactor {
-        fun fetchFood() : Food
+    interface Interactor : BaseInteractor {
+        fun fetchFood()
     }
 }
