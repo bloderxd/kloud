c326b6c6aa2f3c1cf6b47226f4e7edc226e928c4
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-23 17:41:58 -0300

    Implement default favorite food fragment actions

--- app/src/main/java/com/example/bloder/rxmvp/home/ui/fragments/FavoriteFoodFragment.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/ui/fragments/FavoriteFoodFragment.kt
@@ -4,4 +4,6 @@ package com.example.bloder.rxmvp.home.ui.fragments
  * Created by bloder on 22/05/17.
  */
 class FavoriteFoodFragment : BaseMainFragment() {
+    override fun shouldWork(): Boolean = false
+    override fun work() {}
 }
