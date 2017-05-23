2052a3f4ac24309fd304a468fbb896c1be5b8a24
Daniel <wowmegadenis@hotmail.com>
2017-05-23 02:49:58 -0300

    Add view reference as param in food representer

--- app/src/main/java/com/example/bloder/rxmvp/home/representers/FoodRepresenter.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/representers/FoodRepresenter.kt
@@ -1,9 +1,10 @@
 package com.example.bloder.rxmvp.home.representers
 
+import android.support.v4.app.Fragment
 import com.example.bloder.rxmvp.home.identifiers.FoodId
 import com.example.bloder.rxmvp.rx.RxFood
 
 /**
  * Created by bloder on 20/05/17.
  */
-class FoodRepresenter(val id: FoodId) : RxFood.Representer
+class FoodRepresenter(val id: FoodId, val ref: Fragment) : RxFood.Representer
