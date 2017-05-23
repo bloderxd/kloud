d3b11b77facd0b519c02cfb35530c6f7d3dc2465
Daniel <wowmegadenis@hotmail.com>
2017-05-23 02:50:28 -0300

    Start to implement reactive mvp view in main act.

--- app/src/main/java/com/example/bloder/rxmvp/home/ui/activities/MainFoodActivity.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/ui/activities/MainFoodActivity.kt
@@ -1,18 +1,40 @@
-package com.example.bloder.rxmvp.home.ui
+package com.example.bloder.rxmvp.home.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
-import android.widget.FrameLayout
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import com.example.bloder.rxmvp.R
+import com.example.bloder.rxmvp.home.arch.FoodContract
+import com.example.bloder.rxmvp.home.identifiers.ChildId
+import com.example.bloder.rxmvp.home.ui.adapters.FoodViewPagerAdapter
+import com.example.bloder.rxmvp.home.ui.delegates.ViewPagerDelegate
+import com.example.bloder.rxmvp.home.ui.fragments.FavoriteFoodFragment
+import com.example.bloder.rxmvp.home.ui.fragments.FoodFragment
 
-class MainFoodActivity : AppCompatActivity() {
+class MainFoodActivity : AppCompatActivity(), FoodContract.View {
 
+    override val state: HashMap<ChildId, Fragment> = hashMapOf()
+    override var rxTicket by reactive()
     private val tab       by lazy { findViewById(R.id.tab) as TabLayout }
-    private val container by lazy { findViewById(R.id.container) as FrameLayout }
+    private val viewPager by ViewPagerDelegate(this,  R.id.view_pager, FoodViewPagerAdapter(supportFragmentManager))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_food)
+        startCooking()
+    }
+
+    override fun onFoodFetched() {}
+
+    override fun defaultStateInitialization() : HashMap<ChildId, Fragment> = hashMapOf(
+            ChildId.FOOD      to FoodFragment(),
+            ChildId.FAVORITES to FavoriteFoodFragment()
+    )
+
+    private fun startCooking() {
+        tab.setupWithViewPager(viewPager)
+        tab.getTabAt(0)!!.text = "Food"
+        tab.getTabAt(1)!!.text = "My Favorites Food"
     }
 }
