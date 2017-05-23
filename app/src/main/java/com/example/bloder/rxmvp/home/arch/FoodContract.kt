05b274666647ecc94c288ec0fc758d0b87feccb5
Daniel <wowmegadenis@hotmail.com>
2017-05-23 02:49:20 -0300

    Extend rx base view in food contract view protocol

--- app/src/main/java/com/example/bloder/rxmvp/home/arch/FoodContract.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/arch/FoodContract.kt
@@ -1,14 +1,16 @@
 package com.example.bloder.rxmvp.home.arch
 
-import com.example.bloder.rxmvp.base_presenter.RxBasePresenter
+import com.example.bloder.rxmvp.base_arch.RxBasePresenter
+import com.example.bloder.rxmvp.base_arch.RxBaseView
 import com.example.bloder.rxmvp.data.Food
+import com.example.bloder.rxmvp.home.identifiers.ChildId
 
 /**
  * Created by bloder on 20/05/17.
  */
 class FoodContract {
 
-    interface View {
+    interface View : RxBaseView<ChildId> {
         fun onFoodFetched()
     }
 
