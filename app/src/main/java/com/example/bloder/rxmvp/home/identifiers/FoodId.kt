93bca2a9f7f7db3ac8dba55fae0c0b7d0171ef8c
Daniel <wowmegadenis@hotmail.com>
2017-05-25 23:30:28 -0300

    Remove identifiers

--- app/src/main/java/com/example/bloder/rxmvp/home/identifiers/FoodId.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bloder.rxmvp.home.identifiers
-
-/**
- * Created by bloder on 20/05/17.
- */
-enum class FoodId {
-    FETCH_FOOD, ON_FOOD_FETCHED
-}
--- app/src/main/java/com/example/bloder/rxmvp/home/identifiers/fragments/FoodChildViewId.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bloder.rxmvp.home.identifiers.fragments
-
-/**
- * Created by bloder on 23/05/17.
- */
-enum class FoodChildViewId {
-    ON_FOOD_FETCHED
-}
