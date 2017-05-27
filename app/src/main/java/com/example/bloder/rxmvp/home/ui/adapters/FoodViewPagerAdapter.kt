a73ca16de58270042d1d99d9a701e33aeae0fe98
Daniel <wowmegadenis@hotmail.com>
2017-05-27 18:06:09 -0300

    Create state provider field in food vp adapter

--- app/src/main/java/com/example/bloder/rxmvp/home/ui/adapters/FoodViewPagerAdapter.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/ui/adapters/FoodViewPagerAdapter.kt
@@ -3,8 +3,8 @@ package com.example.bloder.rxmvp.home.ui.adapters
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import com.example.bloder.rxmvp.home.arch.FoodContract
 import com.example.bloder.rxmvp.home.representers.state.MainFoodStateRepresenter
-import com.example.bloder.rxmvp.home.ui.fragments.BaseMainFragment
 import com.example.bloder.rxmvp.home.ui.fragments.DessertFragment
 import com.example.bloder.rxmvp.home.ui.fragments.FavoriteFoodFragment
 import com.example.bloder.rxmvp.home.ui.fragments.FoodFragment
@@ -12,27 +12,19 @@ import com.example.bloder.rxmvp.home.ui.fragments.FoodFragment
 /**
  * Created by bloder on 22/05/17.
  */
-class FoodViewPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
-
-    private val fragments by lazy {
-        hashMapOf(
-                MainFoodStateRepresenter.FoodFragmentId()         to  FoodFragment(),
-                MainFoodStateRepresenter.DessertFragmentId()      to  DessertFragment(),
-                MainFoodStateRepresenter.FavoriteFoodFragmentId() to  FavoriteFoodFragment()
-        )
-    }
+class FoodViewPagerAdapter(fragmentManager: FragmentManager, private var stateProvider: FoodContract.View) : FragmentStatePagerAdapter(fragmentManager) {
 
     override fun getItem(position: Int): Fragment = get(position)
-    override fun getCount(): Int = fragments.size
-
-    fun updateState(type : MainFoodStateRepresenter, ref: BaseMainFragment) {
-        fragments[type] = ref
-    }
+    override fun getCount(): Int = stateProvider.state.size
 
     private fun get(position: Int) : Fragment = when (position) {
-            1 -> fragments[MainFoodStateRepresenter.FoodFragmentId()]!!
-            2 -> fragments[MainFoodStateRepresenter.FoodFragmentId()]!!
-            3 -> fragments[MainFoodStateRepresenter.FoodFragmentId()]!!
+            1 -> stateProvider.restoreFrom(MainFoodStateRepresenter.FoodFragmentId())?: Fragment()
+            2 -> stateProvider.restoreFrom(MainFoodStateRepresenter.DessertFragmentId())?:Fragment()
+            3 -> stateProvider.restoreFrom(MainFoodStateRepresenter.FavoriteFoodFragmentId())?:Fragment()
             else -> Fragment()
+    }
+
+    fun updateStateProvider(stateProvider: FoodContract.View) {
+        this.stateProvider = stateProvider
     }
 }
