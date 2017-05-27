2fcd00ed0fbdf021193d8ea3327307796ce7fb8f
Daniel <wowmegadenis@hotmail.com>
2017-05-27 18:10:07 -0300

    Create favorite food fragment class

--- app/src/main/java/com/example/bloder/rxmvp/home/ui/fragments/FavoriteFoodFragment.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/ui/fragments/FavoriteFoodFragment.kt
@@ -1,9 +1,30 @@
 package com.example.bloder.rxmvp.home.ui.fragments
 
+import com.example.bloder.rxmvp.data.Food
+import com.example.bloder.rxmvp.home.arch.FoodContract
+import com.example.bloder.rxmvp.home.representers.fragments.FavoriteFragmentRepresenter
+import com.example.bloder.rxmvp.home.representers.state.MainFoodStateRepresenter
+
 /**
  * Created by bloder on 22/05/17.
  */
-class FavoriteFoodFragment : BaseMainFragment() {
-    override fun shouldWork(): Boolean = false
-    override fun work() {}
+class FavoriteFoodFragment : BaseMainFragment(), FoodContract.FavoriteView {
+
+    override fun addFavoriteFood(food: Food) {
+        updateList(listOf(food))
+    }
+
+    override fun onReceive(event: FavoriteFragmentRepresenter) {
+        when(event) {
+            is FavoriteFragmentRepresenter.AddFavorite -> addFavoriteFood(event.food)
+        }
+    }
+
+    override fun work() {
+        initAdapter()
+        registerReceiver()
+    }
+
+    override fun getStateRepresenter(): MainFoodStateRepresenter.FavoriteFoodFragmentId = MainFoodStateRepresenter.FavoriteFoodFragmentId(this)
+    override fun getRepresenter(): Class<FavoriteFragmentRepresenter> = FavoriteFragmentRepresenter::class.java
 }
