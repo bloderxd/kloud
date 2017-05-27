ef6bed17376cae4e9be433b6e0a56699d1f6960b
Daniel <wowmegadenis@hotmail.com>
2017-05-27 18:01:23 -0300

    Remove description reference in food data class

--- app/src/main/java/com/example/bloder/rxmvp/data/Food.kt
+++ app/src/main/java/com/example/bloder/rxmvp/data/Food.kt
@@ -3,8 +3,4 @@ package com.example.bloder.rxmvp.data
 /**
  * Created by bloder on 21/05/17.
  */
-data class Food(
-        val name: String = "",
-        val description: String = "",
-        val image: String = ""
-)
+data class Food(val name: String = "", val image: String = "")
