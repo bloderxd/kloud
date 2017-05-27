2b34c108509c6348745f8ecdce0123b4723ed20e
Daniel <wowmegadenis@hotmail.com>
2017-05-27 17:59:31 -0300

    Remove rxbaseview reference into provider view

--- app/src/main/java/com/example/bloder/rxmvp/base_arch/mvp/view/ProviderRxBaseView.kt
+++ app/src/main/java/com/example/bloder/rxmvp/base_arch/mvp/view/ProviderRxBaseView.kt
@@ -1,17 +1,18 @@
 package com.example.bloder.rxmvp.base_arch.mvp.view
 
+import com.example.bloder.rxmvp.base_arch.rx.RxBase
 import com.example.bloder.rxmvp.base_arch.state.StateProtocol
 import com.example.bloder.rxmvp.rx.Cloud
 
 /**
  * Created by bloder on 23/05/17.
  */
-interface ProviderRxBaseView<T : Cloud.Representer, out R : StateProtocol.Representer> : RxBaseView<T> {
+interface ProviderRxBaseView<T : Cloud.Representer, out R : StateProtocol.Representer> : RxBase<T> {
 
     fun getStateRepresenter() : R
 
     fun run(action: () -> Any?) {
         action.invoke()
-        cloud.post(getStateRepresenter())
+        cloud().post(getStateRepresenter())
     }
 }
