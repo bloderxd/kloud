39cd755a5a41ad88a7ff13ddb76209d0764793fc
Bloder <aptgetinstalldanielleme@gmail.com>
2017-05-28 10:42:11 -0300

    Add remove action into food list adapter

--- app/src/main/java/com/example/bloder/rxmvp/home/ui/adapters/FoodListAdapter.kt
+++ app/src/main/java/com/example/bloder/rxmvp/home/ui/adapters/FoodListAdapter.kt
@@ -13,7 +13,7 @@ import com.squareup.picasso.Picasso
 /**
  * Created by bloder on 27/05/17.
  */
-class FoodListAdapter(val context: Context, val foods: MutableList<Food>) : RecyclerView.Adapter<FoodListAdapter.ViewHolder>() {
+class FoodListAdapter(val context: Context, val foods: MutableList<Food>, val onStarClicked: (Food) -> Any?) : RecyclerView.Adapter<FoodListAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int = foods.size
 
@@ -21,7 +21,7 @@ class FoodListAdapter(val context: Context, val foods: MutableList<Food>) : Recy
         holder?.bind(foods[position])
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder = ViewHolder(context)
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder = ViewHolder(context, onStarClicked)
 
     fun updateFoods(foods: List<Food>) {
         val oldSize = this.foods.size
@@ -30,10 +30,17 @@ class FoodListAdapter(val context: Context, val foods: MutableList<Food>) : Recy
         this.notifyDataSetChanged()
     }
 
-    class ViewHolder(val context: Context) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(R.layout.view_holder_foods, null)) {
+    fun remove(food: Food) {
+        val index = foods.indexOf(food)
+        foods.remove(food)
+        notifyItemRemoved(index)
+    }
+
+    class ViewHolder(val context: Context, val onStarClicked: (Food) -> Any?) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(R.layout.view_holder_foods, null)) {
         fun bind(food: Food) {
             Picasso.with(context).load(food.image).into(itemView?.findViewById(R.id.food_cover) as ImageView)
             (itemView.findViewById(R.id.food_name) as TextView).text = food.name
+            itemView.findViewById(R.id.star_food).setOnClickListener { onStarClicked.invoke(food) }
         }
     }
 }
