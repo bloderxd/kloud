2a0b8233ab2b4f3f25e1da78d1a6146497a151fb
Daniel <wowmegadenis@hotmail.com>
2017-06-10 17:10:56 -0300

    Change restore from method name

--- app/src/main/java/com/example/bloder/rxmvp/base_arch/state/StateProtocol.kt
+++ app/src/main/java/com/example/bloder/rxmvp/base_arch/state/StateProtocol.kt
@@ -13,7 +13,7 @@ interface StateProtocol<T : StateProtocol.Representer> {
 
     fun prepareStateProtocol() { state = hashMapOf() }
     fun saveState(from: T, ref: Fragment) { state.put(from, ref) }
-    fun restoreFrom(from: T) : Fragment? = state[from]
+    fun restoreStateFrom(from: T) : Fragment? = state[from]
     fun getStateRepresenter() : Class<T>
 
     interface Representer : Cloud.Representer
